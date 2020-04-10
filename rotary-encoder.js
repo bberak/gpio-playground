f49aaa78426e84c237c527b05a59c8d2d7d83b32
bberak <bberak@gmail.com>
2020-04-11 09:41:24 +1000

    Fixed instability with the switch button

--- rotary-encoder.js
+++ rotary-encoder.js
@@ -12,28 +12,30 @@ const createRotaryEncoder = ({
 		const switchButton = new Gpio(buttonPin, {
 			mode: Gpio.INPUT,
 			pullUpDown: Gpio.PUD_UP,
-			edge: Gpio.FALLING_EDGE,
+			alert: true
 		});
+		
+		switchButton.glitchFilter(10000);		
 
 		const _onPushButton = (level) => {
 			if (!level)
 				onPushButton();
 		};
 
-		switchButton.on("interrupt", _onPushButton);
+		switchButton.on("alert", _onPushButton);
 	}
 
 	if (channelAPin && channelBPin) {
 		const channelA = new Gpio(channelAPin, {
 			mode: Gpio.INPUT,
 			pullUpDown: Gpio.PUD_UP,
-			edge: Gpio.RISING_EDGE,
+			edge: Gpio.RISING_EDGE
 		});
 
 		const channelB = new Gpio(channelBPin, {
 			mode: Gpio.INPUT,
 			pullUpDown: Gpio.PUD_UP,
-			edge: Gpio.RISING_EDGE,
+			edge: Gpio.RISING_EDGE
 		});
 
 		const _onClockwise = (a) => {
@@ -57,10 +59,10 @@ const createRotaryEncoder = ({
 let count = 0;
 
 const encoder = createRotaryEncoder({
-	channelAPin: 17,
-	channelBPin: 18,
-	onClockwise: () => console.log(++count),
-	onCounterClockwise: () => console.log(--count),
-	//buttonPin: 17,
-	//onPushButton: () => console.log("onPushButton")
+	//channelAPin: 17,
+	//channelBPin: 18,
+	//onClockwise: () => console.log(++count),
+	//onCounterClockwise: () => console.log(--count),
+	buttonPin: 17,
+	onPushButton: () => console.log(++count)
 });
