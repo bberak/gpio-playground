61db5aabd71aa645f1d6c3a1594047769981cc6b
bberak <bberak@gmail.com>
2020-04-10 15:54:33 +1000

    Trying again

--- rotary-encoder.js
+++ rotary-encoder.js
@@ -1,47 +1,55 @@
-const { Gpio } = require('pigpio');
+const { Gpio } = require("pigpio");
 
-const switchButton = new Gpio(27, { 
-	mode: Gpio.INPUT,
-	pullUpDown: Gpio.PUD_DOWN,
-	edge: Gpio.EITHER_EDGE
-});
+const createRotaryEncoder = ({
+	channelAPin = 17,
+	channelBPin = 18,
+	buttonPin = 27,
+	onPushButton = () => {},
+	onClockwise = () => {},
+	onCounterClockwise = () => {},
+} = {}) => {
+	const switchButton = new Gpio(buttonPin, {
+		mode: Gpio.INPUT,
+		pullUpDown: Gpio.PUD_DOWN,
+		edge: Gpio.EITHER_EDGE,
+	});
 
-switchButton.on('interrupt', level => console.log('switchButton', level, new Date()));
+	switchButton.on("interrupt", onPushButton);
 
-let direction = 'cw';
+	const channelA = new Gpio(channelAPin, {
+		mode: Gpio.INPUT,
+		pullUpDown: Gpio.PUD_UP,
+		edge: Gpio.RISING_EDGE,
+	});
+
+	const channelB = new Gpio(channelBPin, {
+		mode: Gpio.INPUT,
+		pullUpDown: Gpio.PUD_UP,
+		edge: Gpio.RISING_EDGE,
+	});
+
+	const _onClockwise = (a) => {
+		const b = channelB.digitalRead();
+
+		if (a && !b) onClockwise();
+	};
+
+	channelA.on("interrupt", _onClockwise);
+
+	const _onCounterClockwise = (b) => {
+		const a = channelA.digitalRead();
+
+		if (b && !a) onCounterClockwise();
+	};
+
+	channelB.on("interrupt", _onCounterClockwise);
+};
+
 let count = 0;
 
-const channelA = new Gpio(17, {
-	mode: Gpio.INPUT,
-	pullUpDown: Gpio.PUD_UP,
-	edge: Gpio.RISING_EDGE
+const encoder = createRotaryEncoder({
+	channelAPin: 17,
+	channelBPin: 18,
+	onClockwise: () => console.log(++count)),
+	onCounterClockwise: () => console.log(--count)),
 });
-
-const channelB = new Gpio(18, {
-	mode: Gpio.INPUT,
-	pullUpDown: Gpio.PUD_UP,
-	edge: Gpio.RISING_EDGE
-});
-
-const onTwistClockwise = (a) => {	
-	const b = channelB.digitalRead();
-	
-	if (a && !b) {
-		count++;
-		console.log(count);
-	}
-}
-
-
-channelA.on('interrupt', onTwistClockwise);
-
-const onTwistCounterClockwise = (b) => {
-	const a = channelA.digitalRead();
-	
-	if (b && !a) {
-		count--;
-		console.log(count);
-	}
-}
-
-channelB.on('interrupt', onTwistCounterClockwise);
