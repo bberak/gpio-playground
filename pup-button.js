b1360ace3f2d704fa05c72ee5cd2569cfcd840b9
bberak <bberak@gmail.com>
2020-05-26 01:23:25 +1000

    Added some more pins to the pup example and added a reboot script

--- pup-button.js
+++ pup-button.js
@@ -1,6 +1,6 @@
 const { Gpio } = require('pigpio');
 
-[17, 18].forEach(pin => {
+[2, 3, 23, 24, 12, 26, 20].forEach(pin => {
   const button = new Gpio(pin, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_UP,
