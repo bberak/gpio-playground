d692948ac3cb2305e7cd351c744f5946614cd1d2
bberak <bberak@gmail.com>
2020-05-26 14:12:13 +1000

    Added more pins to the button list

--- pup-button.js
+++ pup-button.js
@@ -1,6 +1,6 @@
 const { Gpio } = require('pigpio');
 
-[2, 3, 23, 24, 12, 26, 20].forEach(pin => {
+[2, 3, 23, 24, 12, 26, 20, 4, 17, 18, 27, 22, 10, 9, 25, 11, 8, 5, 21].forEach(pin => {
   const button = new Gpio(pin, {
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_UP,
